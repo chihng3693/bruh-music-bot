da374c865d4baed9df086d7ebd1b5673d7f90d4c
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-09-15 14:36:20 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD
 
 const token = 'ODg3MjA4MzcxNzU1ODMxMzI2.YUAzLw.ps5CyCYawkTz6oijR8JAWY7bU5Y';
 
-const PREFIX = '!';
+const PREFIX = '-';
 
 const fs = require('fs');
 
@@ -29,9 +29,7 @@ client.on('message', message => {
 
     if(command === 'play' || command === 'skip' || command === 'stop'){
         client.commands.get('play').execute(message, args, command, client, Discord);
-    }
-
-    if(command === 'ping'){
+    }else if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     }
 })
