1b8b117f0ff589ccca10c9f7327b13b1bb323d0c
chihng3693 <43714588+chihng3693@users.noreply.github.com>
2021-09-15 15:05:53 +0800

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -125,5 +125,5 @@ const pause_song = (message, server_queue) => {
 const resume_song = (message, server_queue) => {
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
     server_queue.playing = true;
-    server_queue.connection.dispatcher.resume();
+    server_queue.connection.dispatcher.resume(true);
 }
